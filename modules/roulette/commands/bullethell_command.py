e582b5a9a546029b6b90ec1217565266d2cfa65a
Aitor Martin <sakurazaki.setsunita@gmail.com>
2022-07-02 08:11:37 +0900

    remove reporting roulette and bh timeouts

--- modules/roulette/commands/bullethell_command.py
+++ modules/roulette/commands/bullethell_command.py
@@ -11,117 +11,118 @@ from modules.context import CommandContext
 from modules.command import Command
 from log_utils import do_log
 
+
 class BulletHellCommand(Command):
-	"""
-	!bullethell <type|optional|all>
-	!bh
-	!bh all
-	!bh silver
-	"""
+    """
+    !bullethell <type|optional|all>
+    !bh
+    !bh all
+    !bh silver
+    """
 
-	def __init__(self, roulette, permission: str ='mod', dm_keywords: list = list()) -> None:
-		super().__init__(roulette, permission, dm_keywords)
+    def __init__(self, roulette, permission: str ='mod', dm_keywords: list = list()) -> None:
+        super().__init__(roulette, permission, dm_keywords)
 
-	async def execute(self, context: CommandContext) -> None:
+    async def execute(self, context: CommandContext) -> None:
 
-		await do_log(place="guild", data_dict={'event': 'command', 'command': 'bullethell'}, context=context)
+        await do_log(place="guild", data_dict={'event': 'command', 'command': 'bullethell'}, context=context)
 
-		# Check cache and add to cache
-		times = self._module.user_in_cache(context.author['id'])
-		self._module.add_user_to_cache(context.author['id'])
+        # Check cache and add to cache
+        times = self._module.user_in_cache(context.author['id'])
+        self._module.add_user_to_cache(context.author['id'])
 
-		if times:
-			# Let the user know in the channel about the cooldown
-			info_message = f'You may use Roulette command only once a day. \r\n\
-				Next reset cooldown is at <t:{self._module.get_reset_time()}:f> or <t:{self._module.get_reset_time()}:R>'
-			await self._bot.send_embed_dm(context.author['id'], 'Bullet Hell Info', info_message)
-			try:
-				await self._bot.http.delete_message(context.channel_id, context.id, 'Command was on Cooldown')
-			except:
-				# We can just ignore it, who cares.
-				pass
-			return
+        if times:
+            # Let the user know in the channel about the cooldown
+            info_message = f'You may use Roulette command only once a day. \r\n\
+                Next reset cooldown is at <t:{self._module.get_reset_time()}:f> or <t:{self._module.get_reset_time()}:R>'
+            await self._bot.send_embed_dm(context.author['id'], 'Bullet Hell Info', info_message)
+            try:
+                await self._bot.http.delete_message(context.channel_id, context.id, 'Command was on Cooldown')
+            except:
+                # We can just ignore it, who cares.
+                pass
+            return
 
-		# vars
-		rubber = {'name': 'Rubber', 'odds': 2, 'timeout': 3600} # 1h
-		lead = {'name': 'Lead', 'odds': 6, 'timeout': 7200} # 2h
-		silver = {'name': 'Silver', 'odds': 12, 'timeout': 10800} # 3h
-		gold = {'name': 'Gold', 'odds': 24, 'timeout': 14400} # 4h
-		platinum = {'name': 'Platinum', 'odds': 48, 'timeout': 28800} # 8h
-		diamond = {'name': 'Diamond', 'odds': 96, 'timeout': 43200} # 12h
-		obsidian = {'name': 'Obsidian', 'odds': 128, 'timeout': 57600} # 16h
-		cosmic = {'name': 'Cosmic', 'odds': 256, 'timeout': 86400} # 24h
-		shiny = {'name': 'Shiny', 'odds': 4096, 'timeout': 172800} # 48h
+        # vars
+        rubber = {'name': 'Rubber', 'odds': 2, 'timeout': 3600} # 1h
+        lead = {'name': 'Lead', 'odds': 6, 'timeout': 7200} # 2h
+        silver = {'name': 'Silver', 'odds': 12, 'timeout': 10800} # 3h
+        gold = {'name': 'Gold', 'odds': 24, 'timeout': 14400} # 4h
+        platinum = {'name': 'Platinum', 'odds': 48, 'timeout': 28800} # 8h
+        diamond = {'name': 'Diamond', 'odds': 96, 'timeout': 43200} # 12h
+        obsidian = {'name': 'Obsidian', 'odds': 128, 'timeout': 57600} # 16h
+        cosmic = {'name': 'Cosmic', 'odds': 256, 'timeout': 86400} # 24h
+        shiny = {'name': 'Shiny', 'odds': 4096, 'timeout': 172800} # 48h
 
-		bullets = [shiny, cosmic, obsidian, diamond, platinum, gold, silver, lead, rubber]
-		mods = [
-			'468937023307120660', # Moon
-			'186562579412418560', # Amaya
-			'178015162287128576', # Brock
-			'157411268624384002', # Cheeky
-			'177904844412026880', # Duke
-			 '69945889182986240', # Grimace
-			'117373803344035841', # Jestar
-			'187308638321246209', # Kyro
-			 '66292645407760384', # IpN
-			'115843971804037120', # Nyx
-			'184528528572678145', # Shy
-			'109782272663629824', # Sunshine
-			'539881999926689829', # Yui
-		]
+        bullets = [shiny, cosmic, obsidian, diamond, platinum, gold, silver, lead, rubber]
+        mods = [
+            '468937023307120660', # Moon
+            '186562579412418560', # Amaya
+            '178015162287128576', # Brock
+            '157411268624384002', # Cheeky
+            '177904844412026880', # Duke
+             '69945889182986240', # Grimace
+            '117373803344035841', # Jestar
+            '187308638321246209', # Kyro
+             '66292645407760384', # IpN
+            '115843971804037120', # Nyx
+            '184528528572678145', # Shy
+            '109782272663629824', # Sunshine
+            '539881999926689829', # Yui
+        ]
 
-		for bullet in bullets:
-			acc = random.randint(1, bullet['odds'])
+        for bullet in bullets:
+            acc = random.randint(1, bullet['odds'])
 
-			if acc == bullet['odds']:
-				# Player got shot
-				mod = mods[random.randint(0, len(mods)-1)]
-				timeout = int(bullet['timeout']/3600)
+            if acc == bullet['odds']:
+                # Player got shot
+                mod = mods[random.randint(0, len(mods)-1)]
+                timeout = int(bullet['timeout']/3600)
 
-				# Send a smug notification on the channel
-				description = f"<@{context.author['id']}> stands tall in front of the Stormtrooper Mod team, ready to face their destiny.\r\n \
-					**BANG!** A {bullet['name']} bullet shot by <@{mod}> went straight through <@{context.author['id']}>'s skull.\r\n\
-					BACK TO THE PIT for {timeout} hour{'s' if timeout > 1 else ''}"
+                # Send a smug notification on the channel
+                description = f"<@{context.author['id']}> stands tall in front of the Stormtrooper Mod team, ready to face their destiny.\r\n \
+                    **BANG!** A {bullet['name']} bullet shot by <@{mod}> went straight through <@{context.author['id']}>'s skull.\r\n\
+                    BACK TO THE PIT for {timeout} hour{'s' if timeout > 1 else ''}"
 
-				await self._bot.send_embed_message(context.channel_id, "Bullet Hell Loser", description)
+                await self._bot.send_embed_message(context.channel_id, "Bullet Hell Loser", description)
 
-				await asyncio.sleep(10)
+                await asyncio.sleep(10)
 
-				# Default reason
-				reason = 'Automatic timeout issued for losing the bullet hell'
+                # Default reason
+                reason = 'Automatic timeout issued for losing the bullet hell'
 
-				# Issue the timeout
-				timeout_info = self._bot.pitbot_module.add_timeout(user=context.author, guild_id=context.guild.id,
-					time=bullet['timeout'], issuer_id=self._bot.user.id, reason=reason, source='roulette')
+                # Issue the timeout
+                timeout_info = self._bot.pitbot_module.add_timeout(user=context.author, guild_id=context.guild.id,
+                    time=bullet['timeout'], issuer_id=self._bot.user.id, reason=reason, source='roulette')
 
-				# Add the roles
-				for role in context.ban_roles:
-					await self._bot.http.add_member_role(context.guild.id, context.author['id'], role, reason)
+                # Add the roles
+                for role in context.ban_roles:
+                    await self._bot.http.add_member_role(context.guild.id, context.author['id'], role, reason)
 
-				# generate logs in proper channel
-				if context.log_channel:
-					self._module._timeouts.append(f"<@{context.author['id']}> bh {timeout}h")
+                # generate logs in proper channel
+                if context.log_channel:
+                    self._module._timeouts.append(f"<@{context.author['id']}> bh {timeout}h")
 
-				# Send a DM to the user
-				info_message = f"You've been pitted by {context.guild.name} mod staff for {timeout}h for losing the Bullet Hell. \r\n\
-					This timeout doesn't add any strikes to your acount.\r\n\r\n... loser."
+                # Send a DM to the user
+                info_message = f"You've been pitted by {context.guild.name} mod staff for {timeout}h for losing the Bullet Hell. \r\n\
+                    This timeout doesn't add any strikes to your acount.\r\n\r\n... loser."
 
-				await self._bot.send_embed_dm(context.author['id'], "Bullet Hell Loser", info_message)
-				return
+                await self._bot.send_embed_dm(context.author['id'], "Bullet Hell Loser", info_message)
+                return
 
-		# Send a notification on the channel
-		description = f"<@{context.author['id']}> stands tall in front of the Stormtrooper Mod team, ready to face their destiny.\r\n \
-					*thud* *thunk* All the bullets miss the mark hitting the wall behind <@{context.author['id']}>\r\n"
+        # Send a notification on the channel
+        description = f"<@{context.author['id']}> stands tall in front of the Stormtrooper Mod team, ready to face their destiny.\r\n \
+                    *thud* *thunk* All the bullets miss the mark hitting the wall behind <@{context.author['id']}>\r\n"
 
-		await self._bot.send_embed_message(context.channel_id, "Bullet Hell Winner", description)
+        await self._bot.send_embed_message(context.channel_id, "Bullet Hell Winner", description)
 
-	async def send_help(self, context: CommandContext) -> None:
-		"""
-		Sends Help information to the channel
-		"""
+    async def send_help(self, context: CommandContext) -> None:
+        """
+        Sends Help information to the channel
+        """
 
-		fields = [
-			{'name': 'Help', 'value': f"Use {context.command_character}bullethell or {context.command_character}bh to win or lose.", 'inline': False},
-		]
+        fields = [
+            {'name': 'Help', 'value': f"Use {context.command_character}bullethell or {context.command_character}bh to win or lose.", 'inline': False},
+        ]
 
-		await self._bot.send_embed_message(context.channel_id, "Bullet Hell Info", fields=fields)
+        await self._bot.send_embed_message(context.channel_id, "Bullet Hell Info", fields=fields)
--- modules/roulette/roulette.py
+++ modules/roulette/roulette.py
@@ -15,103 +15,104 @@ from .commands import RouletteCommand, BulletHellCommand, ResetRouletteCommand
 
 log: logging.Logger = logging.getLogger("roulette")
 
+
 class Roulette:
 
-	def __init__(self, *, bot: discord.Client) -> None:
-		"""
-		:var bot discord.Client: The bot instance
-		"""
+    def __init__(self, *, bot: discord.Client) -> None:
+        """
+        :var bot discord.Client: The bot instance
+        """
 
-		self._bot = bot
-		self._cache = dict()
-		self._timeouts = list()
-		self._reset_time = datetime.time(20, tzinfo=datetime.timezone.utc)
+        self._bot = bot
+        self._cache = dict()
+        self._timeouts = list()
+        self._reset_time = datetime.time(20, tzinfo=datetime.timezone.utc)
 
-		self.commands = {
-			"roulette": RouletteCommand(self, 'any'),
-			"bullethell": BulletHellCommand(self, 'any'),
-			"resetroulette": ResetRouletteCommand(self, 'mod')
-		}
+        self.commands = {
+            "roulette": RouletteCommand(self, 'any'),
+            "bullethell": BulletHellCommand(self, 'any'),
+            "resetroulette": ResetRouletteCommand(self, 'mod')
+        }
 
-		self.commands['rouiette'] = self.commands['roulette']
-		self.commands['bh'] = self.commands['bullethell']
-		self.commands['rr'] = self.commands['resetroulette']
+        self.commands['rouiette'] = self.commands['roulette']
+        self.commands['bh'] = self.commands['bullethell']
+        self.commands['rr'] = self.commands['resetroulette']
 
-	@tasks.loop(hours=1)
-	async def refresh_cache(self) -> None:
-		if datetime.datetime.now(datetime.timezone.utc).hour == self._reset_time.hour:
-			self._cache = dict()
+    @tasks.loop(hours=1)
+    async def refresh_cache(self) -> None:
+        if datetime.datetime.now(datetime.timezone.utc).hour == self._reset_time.hour:
+            self._cache = dict()
 
-	@tasks.loop(minutes=20)
-	async def report_timeouts(self) -> None:
-		losers = ", ".join(self._timeouts)
-		if losers and len(losers) > 1:
-			await self._bot.send_embed_message(self._bot.default_guild['log_channel'], "Roulette losers", losers)
-		self._timeouts.clear()
+    @tasks.loop(minutes=20)
+    async def report_timeouts(self) -> None:
+        losers = ", ".join(self._timeouts)
+        if losers and len(losers) > 1:
+            await self._bot.send_embed_message(self._bot.default_guild['log_channel'], "Roulette losers", losers)
+        self._timeouts.clear()
 
-	def init_tasks(self) -> None:
-		"""
-		Initialize the different asks that run in the background
-		"""
-		self.refresh_cache.start()
-		self.report_timeouts.start()
+    def init_tasks(self) -> None:
+        """
+        Initialize the different asks that run in the background
+        """
+        self.refresh_cache.start()
+        # self.report_timeouts.start()
 
-	async def handle_commands(self, message: discord.Context) -> None:
-		"""
-		Handles any commands given through the designed character
-		"""
-		
-		command = message.content.replace(self._bot.guild_config[message.guild_id]['command_character'], '')
-		params = list()
+    async def handle_commands(self, message: discord.Context) -> None:
+        """
+        Handles any commands given through the designed character
+        """
+     
+        command = message.content.replace(self._bot.guild_config[message.guild_id]['command_character'], '')
+        params = list()
 
-		if ' ' in command:
-			command, params = (command.split()[0], command.split()[1:])
+        if ' ' in command:
+            command, params = (command.split()[0], command.split()[1:])
 
-		command = command.lower()
-		
-		if command in self.commands:
-			await self.commands[command].execute(CommandContext(self._bot, command, params, message))
-		return
+        command = command.lower()
+       
+        if command in self.commands:
+            await self.commands[command].execute(CommandContext(self._bot, command, params, message))
+        return
 
-	# Functionality
-	def user_in_cache(self, user_id: str) -> Union[dict, bool]:
-		"""
-		Verifies if given user_id is already in cache
-		"""
+    # Functionality
+    def user_in_cache(self, user_id: str) -> Union[dict, bool]:
+        """
+        Verifies if given user_id is already in cache
+        """
 
-		if user_id in self._cache:
-			return self._cache[user_id]
+        if user_id in self._cache:
+            return self._cache[user_id]
 
-		return False
+        return False
 
-	def add_user_to_cache(self, user_id: str) -> None:
-		"""
-		Adds user to cache
-		"""
+    def add_user_to_cache(self, user_id: str) -> None:
+        """
+        Adds user to cache
+        """
 
-		if not user_id in self._cache:
-			self._cache[user_id] = 1
+        if not user_id in self._cache:
+            self._cache[user_id] = 1
 
-		else:
-			self._cache[user_id] += 1
+        else:
+            self._cache[user_id] += 1
 
-	def remove_user_from_cache(self, user_id: str) -> None:
-		"""
-		Removes user from cache
-		"""
+    def remove_user_from_cache(self, user_id: str) -> None:
+        """
+        Removes user from cache
+        """
 
-		if user_id in self._cache:
-			self._cache.pop(user_id)
+        if user_id in self._cache:
+            self._cache.pop(user_id)
 
-	def get_reset_time(self):
-		"""
-		Returns a timestamp reset time
-		"""
+    def get_reset_time(self):
+        """
+        Returns a timestamp reset time
+        """
 
-		# calculate reset time of next day
-		now = datetime.datetime.now(datetime.timezone.utc)
-		reset = datetime.datetime.now(datetime.timezone.utc).replace(hour=self._reset_time.hour, minute=0, second=0, microsecond=0)
-		if now.hour > self._reset_time.hour:
-			reset = reset + datetime.timedelta(days=1)
+        # calculate reset time of next day
+        now = datetime.datetime.now(datetime.timezone.utc)
+        reset = datetime.datetime.now(datetime.timezone.utc).replace(hour=self._reset_time.hour, minute=0, second=0, microsecond=0)
+        if now.hour > self._reset_time.hour:
+            reset = reset + datetime.timedelta(days=1)
 
-		return int(reset.timestamp())
+        return int(reset.timestamp())
