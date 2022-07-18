ddc131de22245fc6020154993794bd1c20a713b1
Aitor Martin <sakurazaki.setsunita@gmail.com>
2022-07-18 23:02:25 +0900

    update sticker_stats updated_date so we know exactly when was the last time the sticker was used

--- database/database.py
+++ database/database.py
@@ -13,6 +13,7 @@ log = logging.getLogger(__name__)
 
 _connection = None
 
+
 def get_conn(path):
     """
     Get a DB object.
@@ -24,7 +25,7 @@ def get_conn(path):
     global _connection   # pylint: disable=global-statement
 
     if _connection is not None:
-    	# Do cleanup if necessary
+        # Do cleanup if necessary
         return _connection
 
     # create a new instance
@@ -36,238 +37,238 @@ def get_conn(path):
 
 class Database:
 
-	def __init__(self, config):
-		"""Create and configure a connection to database.
+    def __init__(self, config):
+        """Create and configure a connection to database.
 
-		Provide with wrapper for database
-		"""
+        Provide with wrapper for database
+        """
 
-		self._db_config = config
-		self._conn = get_conn(config['path'])
-		self._db = self._conn[config['db_name']]
+        self._db_config = config
+        self._conn = get_conn(config['path'])
+        self._db = self._conn[config['db_name']]
 
-	def close(self):
-		"""Close connection to database"""
-		self._conn.close()
+    def close(self):
+        """Close connection to database"""
+        self._conn.close()
 
-	# Server Configuration Related
-	def get_default_guild(self) -> dict:
-		"""
-		Loads the first guild in the list from database
-		"""
+    # Server Configuration Related
+    def get_default_guild(self) -> dict:
+        """
+        Loads the first guild in the list from database
+        """
 
-		col = self._db['discord_config']
+        col = self._db['discord_config']
 
-		guild_info = col.find_one()
+        guild_info = col.find_one()
 
-		return guild_info
+        return guild_info
 
-	async def load_server_configuration(self, guild, bot) -> dict:
-		"""
-		Given a guild, load its configuration
-		"""
+    async def load_server_configuration(self, guild, bot) -> dict:
+        """
+        Given a guild, load its configuration
+        """
 
-		col = self._db['discord_config']
+        col = self._db['discord_config']
 
-		guild_info = col.find_one({'guild_id': guild.id})
+        guild_info = col.find_one({'guild_id': guild.id})
 
-		if guild_info is None:
-			# guild info has the following attrs
-			# 'features', 'icon', 'id', 'name', 'owner', 'permissions'
+        if guild_info is None:
+            # guild info has the following attrs
+            # 'features', 'icon', 'id', 'name', 'owner', 'permissions'
 
-			# Get guild info from discord
-			dc_guild = await bot.http.get_guild(guild.id)
+            # Get guild info from discord
+            dc_guild = await bot.http.get_guild(guild.id)
 
-			# Get default admin roles
-			admin_roles = []
+            # Get default admin roles
+            admin_roles = []
 
-			# roles
-			# {'id': '553455586709078024', 'name': 'Bots', 'permissions': '6546775617', 'position': 2, 'color': 0, 
-			# 'hoist': True, 'managed': False, 'mentionable': False, 'icon': None, 'unicode_emoji': None}
+            # roles
+            # {'id': '553455586709078024', 'name': 'Bots', 'permissions': '6546775617', 'position': 2, 'color': 0, 
+            # 'hoist': True, 'managed': False, 'mentionable': False, 'icon': None, 'unicode_emoji': None}
 
-			for role in dc_guild['roles']:
-				# 1 << 3 == 0x8 == administrator
-				if (int(role['permissions']) & 1 << 3) == 0x8 and not "tags" in role:
-					admin_roles.append(role['id'])
+            for role in dc_guild['roles']:
+                # 1 << 3 == 0x8 == administrator
+                if (int(role['permissions']) & 1 << 3) == 0x8 and not "tags" in role:
+                    admin_roles.append(role['id'])
 
-			guild_info = {
-				'guild_id': guild.id,
-				'name': guild.name,
-				'command_character': bot.config['discord']['default_command_character'],
-				'modmail_character': bot.config['discord']['default_command_character'],
-				'allowed_channels': list(),
-				'denied_channels': list(),
-				'admin_roles': admin_roles,
-				'mod_roles': list(),
-				'ban_roles': list(),
-				'log_channel': None,
-				'modmail_channel': None,
-				'override_silent': False,
-				'auto_timeout_on_reenter': True,
-				'joined_date': datetime.now().isoformat()
-			}
+            guild_info = {
+                'guild_id': guild.id,
+                'name': guild.name,
+                'command_character': bot.config['discord']['default_command_character'],
+                'modmail_character': bot.config['discord']['default_command_character'],
+                'allowed_channels': list(),
+                'denied_channels': list(),
+                'admin_roles': admin_roles,
+                'mod_roles': list(),
+                'ban_roles': list(),
+                'log_channel': None,
+                'modmail_channel': None,
+                'override_silent': False,
+                'auto_timeout_on_reenter': True,
+                'joined_date': datetime.now().isoformat()
+            }
 
-			result = col.insert_one(guild_info)
-			guild_info['_id'] = result.inserted_id
+            result = col.insert_one(guild_info)
+            guild_info['_id'] = result.inserted_id
 
-		return guild_info
+        return guild_info
 
-	def update_server_configuration(self, guild, parameters):
-		"""
-		Given a guild, update its configuration
+    def update_server_configuration(self, guild, parameters):
+        """
+        Given a guild, update its configuration
 
-		@guild: typeof Discord.Guild
-		@parameters: dictionary with values to update
+        @guild: typeof Discord.Guild
+        @parameters: dictionary with values to update
 
-		returns: guild_config
-		"""
+        returns: guild_config
+        """
 
-		col = self._db['discord_config']
+        col = self._db['discord_config']
 
-		guild_info = col.find_one_and_update({'guild_id': guild.id}, {"$set": parameters })
+        guild_info = col.find_one_and_update({'guild_id': guild.id}, {"$set": parameters })
 
-		return guild_info
+        return guild_info
 
-	def add_role_to_guild(self, guild, list_name, parameter):
-		"""
-		Given a guild, add one of its list configuration
+    def add_role_to_guild(self, guild, list_name, parameter):
+        """
+        Given a guild, add one of its list configuration
 
-		@guild: typeof Discord.Guild
-		@list_name: string list name to be updated
-		@parameter: typeof(any) value to be added
+        @guild: typeof Discord.Guild
+        @list_name: string list name to be updated
+        @parameter: typeof(any) value to be added
 
-		returns: guild_config
-		"""
+        returns: guild_config
+        """
 
-		col = self._db['discord_config']
+        col = self._db['discord_config']
 
-		guild_info = col.find_one_and_update({'guild_id': guild.id}, {"$push": {list_name: parameter} })
+        guild_info = col.find_one_and_update({'guild_id': guild.id}, {"$push": {list_name: parameter} })
 
-		return guild_info
+        return guild_info
 
-	def remove_role_from_guild(self, guild, list_name, parameter):
-		"""
-		Given a guild, remove one of its list configuration
+    def remove_role_from_guild(self, guild, list_name, parameter):
+        """
+        Given a guild, remove one of its list configuration
 
-		@guild: typeof Discord.Guild
-		@list_name: string list name to be updated
-		@parameter: typeof(any) value to be added
+        @guild: typeof Discord.Guild
+        @list_name: string list name to be updated
+        @parameter: typeof(any) value to be added
 
-		returns: guild_config
-		"""
+        returns: guild_config
+        """
 
-		col = self._db['discord_config']
+        col = self._db['discord_config']
 
-		guild_info = col.find_one_and_update({'guild_id': guild.id}, {"$pull": {list_name: parameter} })
+        guild_info = col.find_one_and_update({'guild_id': guild.id}, {"$pull": {list_name: parameter} })
 
-		return guild_info
+        return guild_info
 
-	def get_banwords(self, query=None):
-		"""
-		Return all banwords associated with it
+    def get_banwords(self, query=None):
+        """
+        Return all banwords associated with it
 
-		returns: list of Banword
-		"""
+        returns: list of Banword
+        """
 
-		col = self._db['banwords']
+        col = self._db['banwords']
 
-		params = {}
+        params = {}
 
-		if query is not None:
-			params.update(query)
+        if query is not None:
+            params.update(query)
 
-		results = col.find(params)
+        results = col.find(params)
 
-		return list(results)
+        return list(results)
 
-	def store_roles(self, user: dict, guild_id: str) -> Optional[dict]:
-		"""
-		Store roles for a user who is marked to be pruned
-		"""
+    def store_roles(self, user: dict, guild_id: str) -> Optional[dict]:
+        """
+        Store roles for a user who is marked to be pruned
+        """
 
-		col = self._db['prune_users']
+        col = self._db['prune_users']
 
-		user_info = {
-			'user_id': user['user']['id'],
-			'guild_id': guild_id,
-			'roles': user['roles'],
-			'created_date': datetime.now().isoformat()
-		}
+        user_info = {
+            'user_id': user['user']['id'],
+            'guild_id': guild_id,
+            'roles': user['roles'],
+            'created_date': datetime.now().isoformat()
+        }
 
-		result = col.insert_one(user_info)
-		user_info['_id'] = result.inserted_id
+        result = col.insert_one(user_info)
+        user_info['_id'] = result.inserted_id
 
-		return user_info
+        return user_info
 
-	def get_stored_roles(self, user_id: str, guild_id: str) -> Optional[dict]:
-		"""
-		Given a user and a guild check if there are roles stored and return them
-		"""
+    def get_stored_roles(self, user_id: str, guild_id: str) -> Optional[dict]:
+        """
+        Given a user and a guild check if there are roles stored and return them
+        """
 
-		col = self._db['prune_users']
+        col = self._db['prune_users']
 
-		params = {
-			'user_id': user_id,
-			'guild_id': guild_id
-		}
+        params = {
+            'user_id': user_id,
+            'guild_id': guild_id
+        }
 
-		result = col.find_one(params)
+        result = col.find_one(params)
 
-		return result
+        return result
 
-	def delete_stored_roles(self, user_id: str, guild_id: str) -> Optional[dict]:
-		"""
-		Given a user and a guild check if there are roles stored and delete them
-		"""
+    def delete_stored_roles(self, user_id: str, guild_id: str) -> Optional[dict]:
+        """
+        Given a user and a guild check if there are roles stored and delete them
+        """
 
-		col = self._db['prune_users']
+        col = self._db['prune_users']
 
-		params = {
-			'user_id': user_id,
-			'guild_id': guild_id
-		}
+        params = {
+            'user_id': user_id,
+            'guild_id': guild_id
+        }
 
-		result = col.find_one_and_delete(params)
+        result = col.find_one_and_delete(params)
 
-		return result
+        return result
 
 
 if __name__ == '__main__':
-	import pprint
+    import pprint
 
-	config = {
-		'path': 'mongodb://sakurazaki:testpasswd@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
-		'db_name': 'moon2web'
-	}
+    config = {
+        'path': 'mongodb://sakurazaki:testpasswd@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+        'db_name': 'moon2web'
+    }
 
-	# db = new Database()
+    # db = new Database()
 
-	conn = get_conn(config['path'])
-	db = conn[config['db_name']]
-	col = db['users']
+    conn = get_conn(config['path'])
+    db = conn[config['db_name']]
+    col = db['users']
 
-	col.insert_one({
-		'discord_id': 5549,
-		'username': 'Test_user',
-		'username_handle': '0001',
-		'display_name': 'Test User',
-		'avatar': None,
-		'verified': True,
-		'premium_type': None,
-		'created_date': datetime.now().isoformat(),
-		'updated_date': datetime.now().isoformat()
-	})
+    col.insert_one({
+        'discord_id': 5549,
+        'username': 'Test_user',
+        'username_handle': '0001',
+        'display_name': 'Test User',
+        'avatar': None,
+        'verified': True,
+        'premium_type': None,
+        'created_date': datetime.now().isoformat(),
+        'updated_date': datetime.now().isoformat()
+    })
 
-	result = col.find_one({'discord_id': 5549})
-	pprint.pprint(result)
+    result = col.find_one({'discord_id': 5549})
+    pprint.pprint(result)
 
-	col = db['users_bans']
-	results = col.find({'user_id': "539881999926689829"})
+    col = db['users_bans']
+    results = col.find({'user_id': "539881999926689829"})
 
-	for res in results:
-		pprint.pprint(res)
+    for res in results:
+        pprint.pprint(res)
 
-	col = db['users']
-	col.find_one_and_delete({'discord_id': 5549})
+    col = db['users']
+    col.find_one_and_delete({'discord_id': 5549})
 
-	conn.close()
+    conn.close()
--- modules/stickerstats/database.py
+++ modules/stickerstats/database.py
@@ -11,138 +11,142 @@ from typing import Union, List, Optional, Tuple
 
 log: logging.Logger = logging.getLogger("database")
 
+
 class StickerDatabase:
 
-	def __init__(self, *, database):
-		self._db = database._db
+    def __init__(self, *, database):
+        self._db = database._db
 
-	def close(self):
-		self._db.close()
+    def close(self):
+        self._db.close()
 
-	# Timeouts
-	def get_sticker(self, query: dict) -> Optional[dict]:
-		"""
-		Return information about a single sticker.
+    # Timeouts
+    def get_sticker(self, query: dict) -> Optional[dict]:
+        """
+        Return information about a single sticker.
 
-		In the case of being multiple it will always return the first.
-		"""
+        In the case of being multiple it will always return the first.
+        """
 
-		col = self._db['sticker_stats']
+        col = self._db['sticker_stats']
 
-		if query.get("_id"): query['_id'] = ObjectId(query['_id'])
+        if query.get("_id"): query['_id'] = ObjectId(query['_id'])
 
-		result = col.find_one(query)
+        result = col.find_one(query)
 
-		return result
+        return result
 
-	def get_stickers(self, query: dict, sort: Optional[Tuple[str, int]] = None) -> List[dict]:
-		"""
-		Return information about sticker(s)
-		"""
+    def get_stickers(self, query: dict, sort: Optional[Tuple[str, int]] = None) -> List[dict]:
+        """
+        Return information about sticker(s)
+        """
 
-		col = self._db['sticker_stats']
+        col = self._db['sticker_stats']
 
-		if query.get("_id"): query['_id'] = ObjectId(query['_id'])
+        if query.get("_id"): query['_id'] = ObjectId(query['_id'])
 
-		results = col.find(query)
+        results = col.find(query)
 
-		if sort:
-			results = results.sort(sort[0], sort[1])
+        if sort:
+            results = results.sort(sort[0], sort[1])
 
-		results = list(results)
+        results = list(results)
 
-		return results
+        return results
 
-	def create_sticker(self, sticker: dict, guild_id: str, channel_id: Optional[str]) -> dict:
-		"""
-		Create a new sticker in the database.
-		"""
+    def create_sticker(self, sticker: dict, guild_id: str, channel_id: Optional[str]) -> dict:
+        """
+        Create a new sticker in the database.
+        """
 
-		col = self._db['sticker_stats']
+        col = self._db['sticker_stats']
 
-		db_sticker = col.find_one({'id': sticker['id']})
+        db_sticker = col.find_one({'id': sticker['id']})
 
-		if db_sticker is None:
-			db_sticker = {
-				'id': sticker['id'],
-				'name': sticker['name'],
-				'format_type': sticker.get('format_type'),
-				'channels': {
-					channel_id: 1
-				},
-				'created_date': datetime.now().isoformat(),
-				'updated_date': datetime.now().isoformat()
-			}
+        if db_sticker is None:
+            db_sticker = {
+                'id': sticker['id'],
+                'name': sticker['name'],
+                'format_type': sticker.get('format_type'),
+                'channels': {
+                    channel_id: 1
+                },
+                'created_date': datetime.now().isoformat(),
+                'updated_date': datetime.now().isoformat()
+            }
 
-			result = col.insert_one(db_sticker)
-			db_sticker['_id'] = result.inserted_id
+            result = col.insert_one(db_sticker)
+            db_sticker['_id'] = result.inserted_id
 
-		return db_sticker
+        return db_sticker
 
-	def update_sticker(self, *, params: dict, query: dict = None, sticker_id: str = None) -> Optional[dict]:
-		"""
-		Update a sticker from database.
-		"""
+    def update_sticker(self, *, params: dict, query: dict = None, sticker_id: str = None) -> Optional[dict]:
+        """
+        Update a sticker from database.
+        """
 
-		col = self._db['sticker_stats']
+        col = self._db['sticker_stats']
 
-		if query is None and sticker_id is None:
-			raise Exception("query and sticker_id cannot be None")
+        if query is None and sticker_id is None:
+            raise Exception("query and sticker_id cannot be None")
 
-		if query is None:
-			query = dict()
+        if query is None:
+            query = dict()
 
-		if sticker_id is not None:
-			query['id'] = sticker_id
+        if sticker_id is not None:
+            query['id'] = sticker_id
 
-		if query.get("_id"): query['_id'] = ObjectId(query['_id'])
-		if params.get("_id"): params['_id'] = ObjectId(params['_id'])
+        if query.get("_id"): query['_id'] = ObjectId(query['_id'])
+        if params.get("_id"): params['_id'] = ObjectId(params['_id'])
+        params['updated_date'] = datetime.now().isoformat()
 
-		result = col.find_one_and_update(query, {"$set" : params })
+        result = col.find_one_and_update(query, {"$set" : params })
 
-		return result
+        return result
 
-	def update_sticker_stats(self, *, sticker: dict, channel_id: str) -> Optional[dict]:
-		"""
-		Given a sticker_id update the stat of the given channel_id
-		"""
+    def update_sticker_stats(self, *, sticker: dict, channel_id: str) -> Optional[dict]:
+        """
+        Given a sticker_id update the stat of the given channel_id
+        """
 
-		col = self._db['sticker_stats']
+        col = self._db['sticker_stats']
 
-		if channel_id in sticker['channels']:
-			updated_info = col.find_one_and_update(
-				{'id': sticker['id']}, 
-				{"$set": 
-					{f'channels.{channel_id}': sticker['channels'][channel_id]+1}
-				})
+        if channel_id in sticker['channels']:
+            updated_info = col.find_one_and_update(
+                {'id': sticker['id']}, 
+                {"$set": 
+                    {f'channels.{channel_id}': sticker['channels'][channel_id]+1,
+                     'updated_date': datetime.now().isoformat()}
+                })
 
-		else:
-			updated_info = col.find_one_and_update(
-				{'id': sticker['id']}, 
-				{"$set": 
-					{f'channels.{channel_id}': 1}
-				})
+        else:
+            updated_info = col.find_one_and_update(
+                {'id': sticker['id']}, 
+                {"$set": 
+                    {f'channels.{channel_id}': 1,
+                     'updated_date': datetime.now().isoformat()}
+                })
 
-		return updated_info
+        return updated_info
 
-	def delete_sticker(self, *, query: dict = None, sticker_id: str = None) -> Optional[dict]:
-		"""
-		Delete a sticker from database.
-		"""
+    def delete_sticker(self, *, query: dict = None, sticker_id: str = None) -> Optional[dict]:
+        """
+        Delete a sticker from database.
+        """
 
-		col = self._db['sticker_stats']
+        col = self._db['sticker_stats']
 
-		if query is None and user_id is None:
-			raise Exception("query and user_id cannot be None")
+        if query is None and user_id is None:
+            raise Exception("query and user_id cannot be None")
 
-		if query is None:
-			query = dict()
+        if query is None:
+            query = dict()
 
-		if user_id is not None:
-			query['user_id'] = user_id
+        if user_id is not None:
+            query['user_id'] = user_id
 
-		if query.get("_id"): query['_id'] = ObjectId(query['_id'])
+        if query.get("_id"): query['_id'] = ObjectId(query['_id'])
 
-		result = col.find_one_and_delete(query, sort=[('_id', -1)])
+        result = col.find_one_and_delete(query, sort=[('_id', -1)])
 
-		return result
+        return result
