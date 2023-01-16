f7aca761cc1de34bdef08d7555245a6523c972d0
Aitor Martin <sakurazaki.setsunita@gmail.com>
2023-01-16 23:31:28 +0900

    improve watchlist

--- modules/watchlist/commands/watchlist.py
+++ modules/watchlist/commands/watchlist.py
@@ -3,6 +3,8 @@
 ## Watchlist ##
 # Prints the entire watchlist. #
 
+from datetime import datetime
+
 from modules.context import CommandContext
 from modules.command import Command, verify_permission
 from log_utils import do_log
@@ -18,8 +20,10 @@ class WatchListCommand(Command):
         watchlist = self._module.get_watchlist(status="active")
         await do_log(place="guild", data_dict={'event': 'command', 'command': 'watchlist'}, context=context)
         if context.log_channel:
-            userlist = "\r\n".join([f"<@{entry['user_id']}> ({entry['user_id']})\r\n{entry['reason'] or '*No reason given*'}\r\n" 
-                                        for entry in watchlist[:30]])
+            userlist = "\r\n".join([f"<@{entry['user_id']}> ({entry['user_id']})\r\n\
+                                    {entry['reason'] or '*No reason given*'}\r\n\
+                                    {datetime.fromisoformat(entry['created_date']).strftime('%Y-%m-%d')} - <@{entry['issuer_id']}>" 
+                                    for entry in watchlist[:30]])
             if not userlist or len(userlist) == 0:
                 userlist = "*There are no users in the watchlist*"
             await self._bot.send_embed_message(context.log_channel, "Watchlist", userlist)
