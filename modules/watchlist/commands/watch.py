2bddbd9e36b8b6e8d2bb72436df6f11180bb5d29
Aitor Martin <sakurazaki.setsunita@gmail.com>
2023-01-16 23:16:04 +0900

    fixed issue with people not being in the server throwing an error

--- modules/watchlist/commands/watch.py
+++ modules/watchlist/commands/watch.py
@@ -39,14 +39,24 @@ class Watch(Command):
                 user = await self._bot.get_guild(context.guild.id).fetch_member(user)
             elif isinstance(user, dict):
                 user = await self._bot.get_guild(context.guild.id).fetch_member(int(user['discord_id']))
-        except NotFound as ex:
-            print(ex)
+        except NotFound:
+            try:
+                if isinstance(user, int):
+                    user = await self._bot.fetch_user(user)
+                elif isinstance(user, dict):
+                    user = await self._bot.fetch_user(int(user['discord_id']))
+            except NotFound:
+                fields = [
+                    {'name': 'Error', 'value': f"User {user} doesn't exist.", 'inline': True},
+                ]
+                await self._bot.send_embed_message(context.log_channel, "Watch user", color=0xb30000, fields=fields)
+                return
             fields = [
-                {'name': 'Error', 'value': f"User {user} is not in the server and cannot be watched.", 'inline': True},
+                {'name': 'Warning', 'value': f"User {user} is not in the server.", 'inline': True},
             ]
-            await self._bot.send_embed_message(context.log_channel, "Watch user", color=0xb30000, fields=fields)
+            await self._bot.send_embed_message(context.log_channel, "Watch user", color=0xffa500, fields=fields)
+        if not user:
             return
-
         reason = ''
         if len(context.params) >= 1:
             reason = ' '.join(context.params[1:])
