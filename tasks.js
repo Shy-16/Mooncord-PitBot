7bb95f9be225fae5dbd677119591551252d0ef65
calebbabin <object.caleb@gmail.com>
2024-04-18 14:24:11 -0500

    add console output

--- tasks.js
+++ tasks.js
@@ -1,3 +1,5 @@
 const { execSync } = require('child_process');
 
-execSync('poetry run python3 release_task.py');
+console.log("running release task...");
+console.log(execSync('poetry run python3 release_task.py'));
+console.log("release complete");
