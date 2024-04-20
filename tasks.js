d1b81310b6746eab96d0798e2e99512a282b634e
calebbabin <object.caleb@gmail.com>
2024-04-19 20:13:41 -0500

    include environment variables

--- tasks.js
+++ tasks.js
@@ -1,5 +1,7 @@
 const { execSync } = require('child_process');
 
+require('dotenv').config();
+
 console.log("running release task...");
-console.log(execSync('poetry run python3 release_task.py'));
+console.log(execSync('poetry run python3 release_task.py --token ' + process.env.DISCORD_TOKEN));
 console.log("release complete");
