9a7834d845df26e385d1d1d2f1c2791131637354
calebbabin <object.caleb@gmail.com>
2024-04-19 20:15:34 -0500

    try fixing console output

--- tasks.js
+++ tasks.js
@@ -3,5 +3,5 @@ const { execSync } = require('child_process');
 require('dotenv').config();
 
 console.log("running release task...");
-console.log(execSync('poetry run python3 release_task.py --token ' + process.env.DISCORD_TOKEN));
+execSync('poetry run python3 release_task.py --token ' + process.env.DISCORD_TOKEN, {stdio: 'inherit'});
 console.log("release complete");
