ddeb4a4478b3de37b8acef0be1ac474300c9d609
calebbabin <object.caleb@gmail.com>
2024-07-25 18:04:57 -0500

    add expire strike task to rotation

--- tasks.js
+++ tasks.js
@@ -4,4 +4,5 @@ require('dotenv').config();
 
 console.log("running release task...");
 execSync('poetry run python3 release_task.py --token ' + process.env.DISCORD_TOKEN, {stdio: 'inherit'});
+execSync('poetry run python3 expire_strikes_task.py --token ' + process.env.DISCORD_TOKEN, {stdio: 'inherit'});
 console.log("release complete");
